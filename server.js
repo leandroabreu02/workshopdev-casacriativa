96c549e1319ab732e91d65402da6311f5d50bcf5
leandroabreu13 <djleandro.cardoso@gmail.com>
2021-01-09 19:58:56 -0300

    workshopdev project

---
+++ server.js
@@ -0,0 +1,88 @@
+// usei o express para criar e configurar o server
+const express = require("express")
+const server = express()
+
+const db = require("./db")
+
+// configurar arquivos estáticos (css, scripts, imgagens)
+server.use(express.static("public"))
+
+// habilitar uso do req.body
+server.use(express.urlencoded({ extended:true }))
+
+// configuracao do nunkucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+    express: server,
+    noCache: true, // boolean
+})
+
+// criei uma rota /
+// e capturo o pedido do cliente para responder
+server.get("/", function(req, res) {
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if(err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+
+        const reversedIdeas = [...rows].reverse()
+
+        let lastIdeas = []
+        for (let idea of reversedIdeas){
+            if(lastIdeas.length < 2){
+                lastIdeas.push(idea)
+            }
+        }
+
+    return res.render("index.html", {ideas: lastIdeas})
+    })
+})
+
+server.get("/ideias", function(req, res) {
+    
+
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if(err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+
+        const reversedIdeas = [...rows].reverse()
+
+        return res.render("ideias.html", {ideas: reversedIdeas})
+    })
+})
+
+server.post("/", function(req, res){
+//    Inserir dados na tabela
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES(?, ?, ?, ?, ?);
+    `
+
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+    ]
+
+    db.run(query, values, function(err){
+        if(err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+
+        return res.redirect("/ideias")
+    })
+})
+
+// liguei o server na porta 3000
+server.listen(3000)
